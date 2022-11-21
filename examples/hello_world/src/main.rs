142436bd83adc140bd34d62ed7e2c4586d48da8a
Rigidity <me@rigidnetwork.com>
2022-11-20 20:45:05 -0500

    Refactor hello world

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -1,12 +1,9 @@
 use lexer::Lexer;
-use parser::{Output, Parser};
-use syntax::SyntaxNode;
+use parser::Parser;
 
 fn main() {
     let source = "hello";
     let tokens = Lexer::new(source).collect::<Vec<_>>();
-    let Output { green_node, .. } = Parser::parse_tokens(&tokens);
-    let syntax_node = SyntaxNode::new_root(green_node);
-    let debug_tree = format!("{syntax_node:#?}");
-    println!("{}", &debug_tree[0..debug_tree.len() - 1]);
+    let output = Parser::parse_tokens(&tokens);
+    println!("{}", output.debug_tree());
 }
